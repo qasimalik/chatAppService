f832bd6eedcb8c8a42e7d85740393431714bc66d
Qasim Malik <qsmmlk12@gmail.com>
2023-10-17 22:58:43 +0530

    first commit

---
+++ chatApp/server/config/mongo.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+import config from './index.js'
+
+const CONNECTION_URL = "mongodb+srv://kass:kass123@test2023.5tvokvh.mongodb.net"
+
+
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongo has connected succesfully')
+})
+mongoose.connection.on('reconnected', () => {
+  console.log('Mongo has reconnected')
+})
+mongoose.connection.on('error', error => {
+  console.log('Mongo connection has an error', error)
+  mongoose.disconnect()
+})
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongo connection is disconnected')
+})
---
+++ chatApp/server/index.js
@@ -0,0 +1,55 @@
+import http from "http";
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+// routes
+import indexRouter from "./routes/index.js";
+import userRouter from "./routes/user.js";
+import chatRoomRouter from "./routes/chatroom.js";
+import deleteRouter from "./routes/delete.js";
+// middlewares
+import { decode } from './middlewares/jwt.js'
+import {Server, Socket } from "socket.io";
+// mongo connection
+import "./config/mongo.js";
+// socket configuration
+import WebSockets from "./utils/WebSockets.js";
+
+
+const app = express();
+
+/** Get port from environment and store in Express. */
+const port = process.env.PORT || "3000";
+app.set("port", port);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+app.use("/users", userRouter);
+app.use("/room", decode, chatRoomRouter);
+app.use("/delete", deleteRouter);
+
+/** catch 404 and forward to error handler */
+app.use('*', (req, res) => {
+  return res.status(404).json({
+    success: false,
+    message: 'API endpoint doesnt exist'
+  })
+});
+
+/** Create HTTP server. */
+const server = http.createServer(app);
+
+/** Create socket connection */
+const io = new Server(server);
+io.on('connection', WebSockets.connection)
+
+/** Listen on provided port, on all network interfaces. */
+server.listen(port);
+
+/** Event listener for HTTP server "listening" event. */
+server.on("listening", () => {
+  console.log(`Listening on port:: http://localhost:${port}/`)
+});
